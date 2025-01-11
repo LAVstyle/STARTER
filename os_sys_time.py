52fa405973be9263f9cb4df7bc3dabf6db40e1f3
LAVstyle <luchin250398@yandex.ru>
2025-01-11 10:42:41 +0700

    add examples

--- os_sys_time.py
+++ os_sys_time.py
@@ -2,9 +2,9 @@ import os
 import time
 
 def start_file(path):
-    os.startfile(f"{path}")
+    os.startfile(f"C:\Users\AVL\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Visual Studio Code")
     time.sleep(1)
 
 def start_web_site(name):
-    os.system(f"start {name}")
+    os.system(f"start https://calendar.google.com/calendar/u/0/r")
     time.sleep(1)
